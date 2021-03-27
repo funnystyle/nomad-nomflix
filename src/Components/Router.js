7079c260a946a5afb02ce6f7c803cf17612b1d0b
Funnystyle <esero@hanmail.net>
2021-03-27 18:37:46 +0900

    #3.2 CSS in React part Three

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 const router = () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
