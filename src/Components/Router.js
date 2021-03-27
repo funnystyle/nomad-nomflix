f98a5a79b6415333cd78041a58c5cbf8db5b9d04
Funnystyle <esero@hanmail.net>
2021-03-27 17:34:07 +0900

    #2.1 React Router Part One

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 
 const router = () => (
   <Router>
-    <Route path="/" exact component={Home} />
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Route path="/detail" exact component={Detail} />
+    </>
   </Router>
 );
 
