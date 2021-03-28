6aeecd43002879354cccef5ef8e84ec357255fc8
Funnystyle <esero@hanmail.net>
2021-03-29 03:48:50 +0900

    #6.0 Presenter Structure

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,3 +1,16 @@
-const TVContainer = () => "TV";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-export default TVContainer;
+const TVPresenter = ({ topRated, popular, airingToday, error, loading }) =>
+  null;
+
+TVPresenter.PropTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingToday: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default TVPresenter;
