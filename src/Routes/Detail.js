6545d7f8c1e5bbd288c3ca8e1f63675d97fa4741
Funnystyle <esero@hanmail.net>
2021-04-13 16:16:38 +0900

    remove userEffect dependency

--- src/Routes/Detail.js
+++ src/Routes/Detail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { moviesApi, tvApi } from "api";
 import styled from "styled-components";
 import Helmet from "react-helmet";
